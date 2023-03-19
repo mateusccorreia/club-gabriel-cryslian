4f401e70b48b8d7f80c190f749c1d64012b1112d
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-03-18 21:39:58 -0300

    added the Signin component in the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Signin from "./components/Signin";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Club dos músicos</h1>
+      <Signin />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Signin.jsx
@@ -0,0 +1,13 @@
+export default function Signin() {
+  return (
+    <div className="divSignin">
+      <h2>Faça seu login</h2>
+      <input type="text" />
+      <input type="text" />
+      <button>Fazer login</button>
+      <p>
+        Não tem conta? <span>Crie agora!</span>
+      </p>
+    </div>
+  );
+}
